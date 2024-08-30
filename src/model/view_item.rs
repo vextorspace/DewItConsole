6a0b2d9cf5d32473b4e8ea0e2d5770bfed56b1ae
Douglas Ronne <vextorspace@gmail.com>
2024-08-30 10:22:11 -0400

    can now set equality of items by id

--- src/model/view_item.rs
+++ src/model/view_item.rs
@@ -1,9 +1,10 @@
+use uuid::Uuid;
 
-
-#[derive(Debug, Clone, PartialEq, Eq)]
+#[derive(Debug, Clone)]
 pub struct ViewItem {
     pub name: String,
     pub sub_items: Vec<ViewItem>,
+    pub id: String,
 }
 
 impl ViewItem {
@@ -17,9 +18,26 @@ impl ViewItem {
         ViewItem {
             name: name.to_string(),
             sub_items: vec!(),
+            id: Uuid::new_v4().to_string(),
+        }
+    }
+
+    pub fn with_id(name: String, id: String) -> ViewItem {
+        ViewItem {
+            name: name.to_string(),
+            sub_items: vec!(),
+            id,
         }
     }
 }
+
+impl PartialEq for ViewItem {
+    fn eq(&self, other: &Self) -> bool {
+        self.id == other.id
+    }
+}
+
+impl Eq for ViewItem {}
 
 #[cfg(test)]
 mod tests {
@@ -47,5 +65,16 @@ mod tests {
         let item2 = item1.clone();
 
         assert_eq!(item1, item2)
+    }
+
+    #[test]
+    fn view_item_equal_if_id_equal() {
+        let id = String::from("::ITEM_2_ID::");
+        let text = "Item 1".to_string();
+        let item1 = ViewItem::new(text.clone());
+        let item2 = ViewItem::with_id(text.clone(), id.clone());
+        let item3 = ViewItem::with_id("Other Text".to_string(), id.clone());
+        assert_ne!(item1, item2);
+        assert_eq!(item2, item3);
     }
 }
