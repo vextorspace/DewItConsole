19fe8851706f4bfbdc02060ae8b6ba06b303489c
Douglas Ronne <vextorspace@gmail.com>
2024-08-30 10:09:04 -0400

    added a test for model containing items

--- src/model/model.rs
+++ src/model/model.rs
@@ -11,3 +11,18 @@ impl Model {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn model_has_items() {
+        let item1 = ViewItem::new("Item 1".to_string());
+        let item2 = ViewItem::new("Item 2".to_string());
+        let items = vec!(item1.clone(), item2.clone());
+        let model = Model::new(items);
+
+        assert_eq!(vec!(item1, item2), model.items);
+    }
+}
